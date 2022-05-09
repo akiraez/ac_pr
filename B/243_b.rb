e20e1adcb92b93fb6c0e96cb8779f92fe4d3d2c0
akiraez <huigaoqiao3@gmail.com>
2022-05-09 19:12:33 +0900

    fix

--- B/243_b.rb
+++ B/243_b.rb
@@ -9,9 +9,7 @@ uniq_number = [num_array_1, num_array_2].inject(&:&)
 position_array_1 = uniq_number.map { |num| num_array_1.index(num) }
 position_array_2 = uniq_number.map { |num| num_array_2.index(num) }
 
-result = position_array_1.zip(position_array_2).map do |p_1, p_2|
-  p_1 == p_2
-end
+result = position_array_1.zip(position_array_2).map { |p_1, p_2| p_1 == p_2 }
 
 puts result.count(true)
 puts result.count(false)
